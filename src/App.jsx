f0926be6a0e4d136b0fafaadf2730c9cb50dfe8a
lud-ribeiro <rubialudmilla@gmail.com>
2026-01-04 21:49:20 -0300

    Projeto React mapa de mobilidade - versão inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./componentes/Header";
+import LinesList from "./pages/LinesList";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <LinesList />
+    </>
+  );
+}
+
+
+
+
---
+++ src/componentes/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ padding: "10px", background: "#1976d2", color: "#fff" }}>
+      <h1>Linhas de Ônibus - Semob</h1>
+    </header>
+  );
+}
