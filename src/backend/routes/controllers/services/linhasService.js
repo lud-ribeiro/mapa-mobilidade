33e0ff077e3ccdfc1d07cf3ad1e23d28a7824e0c
lud-ribeiro <rubialudmilla@gmail.com>
2026-01-14 10:11:21 -0300

    Delete src/backend/routes/controllers directory

--- src/backend/routes/controllers/services/linhasService.js
+++
@@ -1,8 +0,0 @@
-const pool = require("../database/db");
-
-async function buscarLinhas() {
-  const result = await pool.query("SELECT * FROM linhas");
-  return result.rows;
-}
-
-module.exports = { buscarLinhas };
